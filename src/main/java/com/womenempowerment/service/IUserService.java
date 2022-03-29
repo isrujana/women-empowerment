d4cb6d0844029945f34a42d56f23dc99fd3636f1
isrujana <50314621+isrujana@users.noreply.github.com>
2022-03-29 09:58:21 +0530

    Update IUserService.java

--- src/main/java/com/womenempowerment/service/IUserService.java
+++ src/main/java/com/womenempowerment/service/IUserService.java
@@ -3,7 +3,7 @@ package com.womenempowerment.service;
 import com.womenempowerment.model.User;
 
 public interface IUserService  {
-	User registerUser(User user);
+	void registerUser(User user);
 //	User login(String username, String password);
 //	User forgotPassword(User user);
 }
